245c961d85f1950189f3d31040dcf340aa12a2f4
מרים שכטר < @KITA-5-12>
2024-02-22 16:30:57 +0200

    Add project files.

---
+++ Genery/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Genery;
+
+
+
+Console.WriteLine("Hello, World!");
+static void printObj<T>(T obj)
+{
+    Console.WriteLine(obj);
+}
+//printObj<int>(5);
+//printObj<string>("exelent");
+SwapList<int> swapList = new SwapList<int>();
+swapList.Add(5);
+swapList.Add(12);
+swapList.Add(13);
+Console.WriteLine(swapList[0]+" "+swapList[1] +" "+ swapList[2]);
+swapList.Swaper(1, 2);
+Console.WriteLine(swapList[0] + " " + swapList[1] + " " + swapList[2]);
---
+++ Genery/SwapList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genery
+{
+    public class SwapList<T>:List<T>
+    {
+       public SwapList() { }
+       public void Swaper(int i1, int i2) 
+        { 
+            T temp = this[i1];
+            this[i1] = this[i2];
+            this[i2] = temp;
+        }
+    }
+}
